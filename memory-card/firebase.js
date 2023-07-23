d4fc3acb01c416e631270f23f6d95650bef0dc4a
BaoWoW <baonguyen9392@gmail.com>
2023-07-22 17:01:34 -0700

    Host & Deploy With Firebase

---
+++ memory-card/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgX6GUFNs3eEPWSmBbr_ZFsyLERGQW_28",
+  authDomain: "memorycard-4cda5.firebaseapp.com",
+  projectId: "memorycard-4cda5",
+  storageBucket: "memorycard-4cda5.appspot.com",
+  messagingSenderId: "987374200615",
+  appId: "1:987374200615:web:4b80b72c6df27a2a156b02"
+};
+
+const app = initializeApp(firebaseConfig);
